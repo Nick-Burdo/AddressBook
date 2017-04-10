3ca34d47854867518e1b60c590eb04f3105173cf
Nick Burdo <n.burdo@iterios.com>
2017-04-10 21:14:34 +0300

    JavaFX-19 Person method toString

--- src/ru/javabegin/address_book/objects/Person.java
+++ src/ru/javabegin/address_book/objects/Person.java
@@ -1,33 +1,50 @@
 package ru.javabegin.address_book.objects;
 
+import javafx.beans.property.SimpleStringProperty;
+
 /**
  * Created by tigra on 09.04.17.
  */
 public class Person {
-    private String name;
-    private String phone;
+    private SimpleStringProperty name;
+    private SimpleStringProperty phone;
 
     public String getName() {
+        return name.get();
+    }
+
+    public SimpleStringProperty nameProperty() {
         return name;
     }
 
     public void setName(String name) {
-        this.name = name;
+        this.name.set(name);
     }
 
     public String getPhone() {
+        return phone.get();
+    }
+
+    public SimpleStringProperty phoneProperty() {
         return phone;
     }
 
     public void setPhone(String phone) {
-        this.phone = phone;
+        this.phone.set(phone);
     }
 
     public Person(String name, String phone) {
-        this.name = name;
-        this.phone = phone;
+        this.name = new SimpleStringProperty(name);
+        this.phone = new SimpleStringProperty(phone);
+    }
+    public Person() {
     }
 
-    public Person() {
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
     }
 }
